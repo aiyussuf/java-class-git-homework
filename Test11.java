d94765e1d37b120090d3fa6622021d3b53386346
Abdirahman Yussuf <aiyussuf@hotmail.com>
2018-02-03 15:44:02 -0700

    Just tried to add a new file

---
+++ Test11.java
@@ -0,0 +1,23 @@
+
+
+import java.util.*;
+
+class Random
+
+{
+
+Random (int n)
+ {
+int i =	r.nextInt(int n);
+
+}
+
+public static void main (String[] args)
+{
+Random r = new Random();
+System.out.println(r.i);
+
+}
+
+
+}
