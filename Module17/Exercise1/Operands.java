2a9498a897f967e19fb7b234a190b250377f01ae
Abdirahman Yussuf <aiyussuf@hotmail.com>
2018-04-19 04:50:26 -0600

    Final Exercise1

---
+++ Module17/Exercise1/Operands.java
@@ -0,0 +1,39 @@
+/*
+Using the file Operands linked in the homework, create an instance of the class Operands and invoke the method executBinaryOperation twice. Once with the a lambda expression implementing addition and once with a lambda expression implementing multiplication.
+Hint: The method executeBinaryOperation expects a Lambda backed by the built-in Functional Interface DoubleBinaryOperator which is defined like this:
+*/
+
+import java.util.function.DoubleBinaryOperator;
+
+
+public class Operands {
+    private double operand1;
+    private double operand2;
+
+    public Operands(double operand1, double operand2) {
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+    }
+
+
+    public double executBinaryOperation(String operationName, DoubleBinaryOperator operation) {
+        double answer = operation.applyAsDouble(operand1, operand2);
+        System.out.println("Executing " + operationName + " on " + operand1
+                + " and " + operand2 + " resulted in " + answer);
+        return answer;
+    }
+
+
+  public static void main(String[] args) {
+    Operands operation = new Operands(4.0,5.0);
+    operation.executBinaryOperation("addition", (double x, double y) -> x + y);
+    DoubleBinaryOperator add = (double x, double y) -> x + y;
+		System.out.println( "Addition: " + add.applyAsDouble( 4.0, 5.0));
+
+    operation.executBinaryOperation("multiplication", (double x, double y) -> x * y);
+    DoubleBinaryOperator multiply = (double x, double y) -> x * y;
+		System.out.println( "Multiplication: " + multiply.applyAsDouble( 4.0, 5.0));
+
+
+    }
+}
